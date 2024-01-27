ecb8763ce0061448c969900482acd923cccdb4ad
unknown <karimicna@gmail.com>
2024-01-27 17:39:24 +0330

    the whole thing

---
+++ elevate/crm/urls.py
@@ -0,0 +1,29 @@
+
+from django.urls import path
+
+from . import views
+
+
+urlpatterns = [
+
+    path('', views.homepage, name=''),
+    
+    path('create-task', views.create_task, name='create-task'),
+    
+    path('view-tasks', views.tasks, name='view-tasks'),
+    
+    path('update-task/<str:pk>', views.update_task, name='update-task'),
+    
+    path('delete-task/<str:pk>', views.delete_task, name='delete-task'),
+    
+    path('register', views.register, name='register'),
+    
+    path('my-login', views.my_login, name='my-login'),
+    
+    path('dashboard', views.dashboard, name='dashboard'),
+    
+    path('user-logout', views.user_logout, name= 'user-logout')
+    
+
+    
+]
